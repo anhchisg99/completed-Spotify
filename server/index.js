39b87012ce8dacda9eb3b6ebacdb0bdb3ae40fe2
chi_duong <anhchictw311@gmail.com>
2022-12-07 12:14:11 +0700

    fix for deploying aws

--- server/index.js
+++ server/index.js
@@ -21,14 +21,17 @@ app.use(cors())
 //   res.send('Hello World!')
 // })
 //check
+app.use(express.static(__dirname + '/public/'));
 
-if (process.env.NODE_ENV === 'production') {
-    // Static folder
-    app.use(express.static(__dirname + '/public/'));
+// for heroku
+
+// if (process.env.NODE_ENV === 'production') {
+//     // Static folder
+//     app.use(express.static(__dirname + '/public/'));
   
-    // Handle SPA
-    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
- }
+//     // // Handle SPA
+//     app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+// }
 
 
 app.listen(port, () => {
